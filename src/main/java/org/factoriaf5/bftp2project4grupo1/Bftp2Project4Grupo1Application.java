d12df46dcb826d509f31a8b4cccd4b9bc4565ed8
Sarama3118 <ramissara@gmail.com>
2021-12-14 12:48:13 +0100

    first commit

---
+++ src/main/java/org/factoriaf5/bftp2project4grupo1/Bftp2Project4Grupo1Application.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.bftp2project4grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp2Project4Grupo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp2Project4Grupo1Application.class, args);
+    }
+
+}
