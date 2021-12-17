81bab77280b2dae31e89c6f6e7d42ca6bd5246a4
Sarama3118 <ramissara@gmail.com>
2021-12-17 09:21:49 +0100

    arreglado html de home para que salgan las cards

--- src/main/java/org/factoriaf5/bftp2project4grupo1/repository/SampleDataLoader.java
+++ src/main/java/org/factoriaf5/bftp2project4grupo1/repository/SampleDataLoader.java
@@ -19,7 +19,7 @@ public class SampleDataLoader {
     @PostConstruct
     public void loadSampleData() {
         gameRepository.saveAll(List.of(
-                new Game("Wii Sports","Wii",2006,"https://static.wikia.nocookie.net/videojuego/images/9/98/WiiSport_BA-1-.jpg/revision/latest/top-crop/width/360/height/450?cb=20070629185312"),
+                new Game("Wii Sports","Wii",2006,""),
                 new Game("Super Mario Bros", "NES", 1985, "https://i.blogs.es/638318/super_mario_bros_logo/450_1000.jpeg"),
                 new Game("Mario Kart Wii", "Wii", 2008, "https://cdn.atomix.vg/wp-content/uploads/2014/02/PalMK8Box.jpg"),
                 new Game("Wii Sports Resort", "Wii", 2009, "https://m.media-amazon.com/images/I/51o5CRXYcXL._AC_.jpg"),
